09c161dc8674e71d583d5185f93f52cfde5605d2
Razia Ebrahimi <razia.ebrahimi706@gmail.com>
2020-05-02 05:49:35 -0700

    first update/login(java codes)

---
+++ app/src/main/java/com/auaf/gapp/ForgotPassword.java
@@ -0,0 +1,14 @@
+package com.auaf.gapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password_activity);
+    }
+}
