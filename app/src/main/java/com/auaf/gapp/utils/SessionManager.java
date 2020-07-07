139d2c56f1b96460213845af3f17f1f3351d656d
faisalhakim <malikfaisalhakim@gmail.com>
2020-07-07 17:05:40 +0430

    Commit 07- MainPage, Post Data Main Fragment

--- app/src/main/java/com/auaf/gapp/utils/SessionManager.java
+++ app/src/main/java/com/auaf/gapp/utils/SessionManager.java
@@ -16,6 +16,8 @@ public class SessionManager {
 
     private String IS_LOGGED_IN="IS_LOGGED_IN";
     private String IS_FIRST_TIME ="IS_FIRST_TIME";
+    private String NAME_USER ="NAME_USER";
+    private String PHOTO_USER ="PHOTO_USER";
 
 
 
@@ -35,15 +37,55 @@ public class SessionManager {
 
 
 
- public void setHighScore(int score){
+    public void setHighScore(int score){
         editor.putInt(HIGH_SCORE,score);
         editor.commit();
- }
+    }
 
 
- public int getHighScore(){
+
+
+    public int getHighScore(){
         return preferences.getInt(HIGH_SCORE,0);
- }
+    }
+
+
+
+    public void setNameUser(String name){
+        editor.putString(NAME_USER,name);
+        editor.commit();
+    }
+
+
+
+
+    public String getNameUser(){
+        return preferences.getString(NAME_USER,"User");
+    }
+
+    public void setPhotoUSER(String photo){
+        editor.putString(PHOTO_USER,photo);
+        editor.commit();
+    }
+
+
+
+
+    public String getPHOTO_USER(){
+        return preferences.getString(PHOTO_USER,"photo");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     public void setLoggedIn(boolean login){
